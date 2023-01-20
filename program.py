0e5a8c6837e9d906c2313df96928d8bdc811ca42
pmalec1 <01169056@pw.edu.pl>
2023-01-20 12:17:52 +0100

    Import packages

--- program.py
+++ program.py
@@ -1,1 +1,10 @@
+try:
+    import os.path
+except:
+    print("Library os not found, please consider reinstalling Python enviroment")
 
+
+try:
+    import sys
+except:
+    raise Exception("Library sys not found, please consider reinstalling Python enviroment")
